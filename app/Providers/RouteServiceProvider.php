51bfcca5a22aa455e7d51f95b82b01662a366571
Dima <dmytrotus@gmail.com>
2020-10-09 13:54:28 +0300

    fixed redirects

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-    public const DASHBOARD = 'dashboard/customers';
+    public const HOME = 'dashboard/customers';
 
     /**
      * The controller namespace for the application.
